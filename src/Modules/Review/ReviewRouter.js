cc35981666c765426a136199abf7a88df1b41f8e
salah <salahsaamir707@gmail.com>
2024-03-28 17:20:45 +0200

    update user in review

--- src/Modules/Review/ReviewRouter.js
+++ src/Modules/Review/ReviewRouter.js
@@ -3,5 +3,7 @@ import { AuthMiddleware, Roles } from "../../Middleware/Auth.js";
 import * as  Controller from './Controller/review.js'
 export const ReviewRouter=Router()
 
+
 ReviewRouter.route('/').get(Controller.GetAllReview)
-ReviewRouter.route('/:attraction').get(Controller.GetSpecificReview).post(AuthMiddleware(Roles.admin),Controller.AddReview)
+
+ReviewRouter.route('/:attraction').get(Controller.GetSpecificReview).post(AuthMiddleware(Roles.user),Controller.AddReview)
